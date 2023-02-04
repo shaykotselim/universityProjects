7531d52c7f33bfa38ece97635de4f170d509f7aa
shaykot43500 <hossainshaykot@gmail.com>
2023-02-04 11:10:27 +0600

    first commit

---
+++ uufirst/first.c
@@ -0,0 +1,21 @@
+/*
+        Uttara University Depertment of Computer of Science & Engineering
+        Name: Md Shaykot Hossain Selim
+        ID: 2231091023
+        Section: A
+
+*/
+            // Define Section
+
+        #include<stdio.h>
+
+
+            // Global Declaration
+        int main(void){
+            printf("Name: Md Shaykot Hossain Selim\n");
+            printf("ID: 2231091023\t");
+            printf("Batch: 59\t");
+            printf("Section: A");
+            return 0;
+
+        }
