c7b0e18d3fdaad28b985d8888fc8bbd5b6b0bade
shaykot43500 <hossainshaykot@gmail.com>
2023-02-04 12:14:33 +0600

    added secondTask

---
+++ secondTask/task2.c
@@ -0,0 +1,16 @@
+/*              UTTARA UNINVERSTY DEPERTMENT OF CSE
+                Name: MD SHAYKOT HOSSAIN SELIM
+                STUDENT ID: 2231091023
+                BATCH: 59
+                SECTION: A
+*/
+
+            #include<stdio.h>
+
+            int main(void){
+                printf("      ,\n");
+                printf("     @\n");
+                printf("*   #\n");
+                printf(" / \\ \n");
+                printf("  +");
+            }
